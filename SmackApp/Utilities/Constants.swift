51b14f08eddc4220a9521cecd7c74945c15316e1
Victor Mihaita <vmihai12@icloud.com>
2018-01-19 22:34:16 +0100

    regUser function-Alamofire

--- SmackApp/Utilities/Constants.swift
+++ SmackApp/Utilities/Constants.swift
@@ -8,8 +8,19 @@
 
 import Foundation
 
+//URL constants
+let BASE_URL = "https://vmihaichatapp.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "logedIn"
+let USER_EMAIL = "userEmail"
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
