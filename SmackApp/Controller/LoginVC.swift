ca750ee858fd7f25dd4d286e9fb0a4c50c7b6153
Victor Mihaita <vmihai12@icloud.com>
2018-01-19 20:45:32 +0100

    Login View

---
+++ SmackApp/Controller/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  SmackApp
+//
+//  Created by Victor Mihaita on 19/01/2018.
+//  Copyright © 2018 Victor Mihaita. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func closePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ SmackApp/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SmackApp
+//
+//  Created by Victor Mihaita on 19/01/2018.
+//  Copyright © 2018 Victor Mihaita. All rights reserved.
+//
+
+import Foundation
+
+
+//Segues
+let TO_LOGIN = "toLogin"
