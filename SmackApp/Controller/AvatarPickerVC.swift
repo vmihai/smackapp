e645ba6ac2c50bb2fa65b6d0ceac5ce076a6f854
Victor Mihaita <vmihai12@icloud.com>
2018-01-20 13:12:04 +0100

    Finish avatar selection, and save it to the user instance

--- SmackApp/Controller/AvatarPickerVC.swift
+++ SmackApp/Controller/AvatarPickerVC.swift
@@ -38,12 +38,38 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 28
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var numOfColumns: CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {numOfColumns = 4}
+        let spaceBetweenCells: CGFloat = 10
+        let padding: CGFloat = 40
+        let cellDimention = ((collectionView.bounds.width - padding) - (numOfColumns - 1) * spaceBetweenCells) / numOfColumns
+        
+        return CGSize(width: cellDimention, height: cellDimention )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        } else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 
     @IBAction func backPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func segmentControlChanged(_ sender: Any) {
+        if segmentControll.selectedSegmentIndex == 0 {
+            avatarType = AvatarType.dark
+        } else {
+            avatarType = AvatarType.light
+        }
+        collectionView.reloadData()
     }
     
 }
--- SmackApp/Controller/CreateAccountVC.swift
+++ SmackApp/Controller/CreateAccountVC.swift
@@ -24,6 +24,12 @@ class CreateAccountVC: UIViewController {
         super.viewDidLoad()
         
     }
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
     
     @IBAction func createBtnPressed(_ sender: UIButton) {
         
